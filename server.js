698a27df1e935c1de5442224e1a9d6bf7eb9be01
mohamed-ahmed101 <mohamed.ahmedelgawad@gmail.com>
2021-06-08 22:11:32 +0200

    finalize server

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = { };
 
-// Require Express to run server and routes
-
+const express = require('express');
+const PORT = process.env.PORT || 3000
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Cors for cross origin allowance
-
+app.use(require('cors')())
 // Initialize the main project folder
 app.use(express.static('website'));
 
+app.get('/getAll', (req, res) => {
+    res.json(projectData);
+})
 
+app.post('/addData', (req, res) => {
+    let { temperature, date, user_response } = req.body;
+    if (!(temperature && date && user_response)) return res.status(400).send("enter valid body");
+    projectData = {temperature, date, user_response};
+    return res.json({status:"true" , description : "new data added"});
+})
 // Setup Server
+app.listen(PORT, () => {
+    console.log(`server up and running on port ${PORT}`)
+})
